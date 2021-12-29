b1cbf3b863d7bc18b7f1e74195cde502eaa6b165
Glauber Vilane Ferreira Marques <gvfmarques@hotmail.com>
2021-12-29 12:16:30 -0600

    First Controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App";
+            ViewData["Author"] = "Glauber Marques";
 
             return View();
         }

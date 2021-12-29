c2ea87f09b117c7b8065688d4a3c05290fe192ae
Glauber Vilane Ferreira Marques <gvfmarques@hotmail.com>
2021-12-29 15:32:45 -0600

    Feat:Seeding Service

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -14,7 +14,7 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public Department(int id, string name, ICollection<Seller> sellers)
+        public Department(int id, string name)
         {
             Id = id;
             Name = name;

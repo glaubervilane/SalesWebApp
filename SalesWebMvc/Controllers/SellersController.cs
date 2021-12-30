7f337914d1e53ccae81c20bf9be6ac58e3f8a45c
Glauber Vilane Ferreira Marques <gvfmarques@hotmail.com>
2021-12-29 18:25:30 -0600

    Feat:Adding Details view

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -79,5 +79,20 @@ namespace SalesWebMvc.Controllers
             }
         }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = await _sellerService.FindByIdAsync(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
